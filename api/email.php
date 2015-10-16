c0aed8977d1d3143d618ee347c90771e4a442d86
Andy Walz <andywalz@gmail.com>
2015-10-16 11:51:10 -0500

    Email bug fix

--- api/email.php
+++ api/email.php
@@ -2,17 +2,17 @@
 
 if($_POST['skey'] == "Vdb2PwCgMQsEVV3jWfLvqEMLeXchevqq") {
 
-	//$to = "solarp@umn.edu";
 	$to = $_POST['to'];//"andywalz@gmail.com";
 	$to_name = $_POST['to_name'];//"Bad Data Czar";
 	$subject = $_POST['subject'];//"Bad Data Notification";
-	//$body = "<br>This location may need to be reprocessed: <a href='http://solar.maps.umn.edu/app/index.html?lat=" . $_REQUEST['lat'] . "&long=" . htmlentities($_REQUEST['long']) ."'>" . $_REQUEST['lat'] . ", " . $_REQUEST['long'] . "</a><br><br>" . $_REQUEST['notes']. "<br><br>Submitted by: ".$_REQUEST['name']." - ".$_REQUEST['email']."<br><br>";
+	
 	$body = $_POST['body'];//"Test 123"
-	$replytoaddress = $_REQUEST['from_email'];
-	$fromname = $_REQUEST['from_name'];
+	$replytoaddress = $_POST['from_email'];
+	$fromname = $_POST['from_name'];
+	$fromaddress="mnsolarsuitability@gmail.com";
 
-	$result = send_email($to, $to_name, $body, $subject, $fromaddress="mnsolarsuitability@gmail.com", $fromname, $replytoaddress);	
-
+	$result = send_email($to, $to_name, $body, $subject, $fromaddress, $fromname, $replytoaddress);
+	
 	if($result == "Message sent!") {
 		if (version_compare(PHP_VERSION, '5.2.0') >= 0) {
 			$response_arr = array('success' => $result);
